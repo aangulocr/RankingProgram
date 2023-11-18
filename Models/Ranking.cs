8c7b996fff031fb0f81818178c7a2c82f0cf68db
aangulo <aangulocr@gmail.com>
2023-11-18 17:09:16 -0600

    Actualizacion PUll

---
+++ Models/Ranking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RankingProgram.Models
+{
+    public class Ranking
+    {
+        public int Id { get; set; }
+
+        public int? Posicion { get; set; }
+
+        public string LenguajeProgramacion { get; set; }
+
+        public double Puntos { get; set; }
+
+        public double? Diferencia { get; set; }
+    }
+}
